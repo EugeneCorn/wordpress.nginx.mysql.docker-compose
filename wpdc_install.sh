c3d6f31816e1fd7b72f2aec6c5153320068543e9
Eugene_Corn <madcallisto@gmail.com>
2022-05-18 11:27:59 +0300

    edit string Set up stable repository

--- wpdc_install.sh
+++ wpdc_install.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 
 # Make update of repositories
 
@@ -14,8 +13,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 
 # Set up the stable repository
 
-echo \ 
-"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+ echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Installing Docker Engine
